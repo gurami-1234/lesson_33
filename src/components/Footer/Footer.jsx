477d715f7ace16e888a0d99d5b239707cd05ddf2
gurami-1234 <gukadavitadze22@gmail.com>
2024-10-13 19:56:44 +0400

    Footer (Copyright)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <hr />
-      <span >Copyright &#xA9; Next Step Academy</span>
+      <span >Copyright &#xA9; Next Step Academy (Team)</span>
       <hr />
     </footer>
   )
